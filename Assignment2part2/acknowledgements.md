a43f4f8ff05496bc4b12e293a08c8fbb3e3ece7a
carlkaziboni <carlkazibonicman@hotmail.com>
2024-03-08 18:30:26 +0000

    Acknowledgements and Design.java final

--- Assignment2part2/acknowledgements.md
+++ Assignment2part2/acknowledgements.md
@@ -1,63 +1,62 @@
 # Acknowledgements #
 
-- Name: TODO Your name
-- Student ID: TODO Your student ID
-- Tutorial group: TODO Your tutorial group
-- Tutor: TODO Your tutor's name
-- Date: TODO Today's date in ISO 8601 format YYYY-MM-DD
+- Name: Carl Kaziboni
+- Student ID: s2413182
+- Tutorial group: 2
+- Tutor: Andrey Demindenko
+- Date: 2024-03-07
+
+## People ##
+
+| Person | Role | Help |
+| ------ | -----| ---- |
+| Godfrey Antomarlin | Classmate | Gave advice on how to implement get income function using while loops to obtain correct input |
+| Agam Cohen | Classmate | Helped test all the cases that were possible for Taxes |
+| Charlotte Baker | Classmate | Helped with Javadoc writing for Design and GetNumerical classes, as well as for functions in each of the functions |
+| Andrey Demindenko | Tutor | Gave advice on how to modularize code |
+| Fiona Mcneill | Lecturer | Explained how Scanner class works and how to use it to get input |
+| [Code golf answer in C](https://codegolf.stackexchange.com/a/256253) | Code golf answer | Inspire on how to calucate taxes for each band |
+
+## Tools ##
+| Tool | Category | Help |
+| ---- | -------- | ---- |
+| [WebAIM Contrast Checker](https://webaim.org/resources/contrastchecker/) | Accessibility | Check colors contrast to make easier to view and more accessible |
+| [Color Blindness Simulator](https://www.color-blindness.com/coblis-color-blindness-simulator/)| Accessiblity | Simulate color blindness in order to see if everything is accessible |
+| [IntelliJ Markdown Previewer](https://www.jetbrains.com/help/idea/markdown.html#preview) | Markdown Writing | Preview Markdown as it was being written |
+| [IntelliJ IDEA](https://www.jetbrains.com/idea/) | Programming | Write all code and markdown |
+
+## Sources ##
+| Source | Category | Help |
+| ------ | -------- | ---- |
+| [StackOverflow Blank Line Check](https://stackoverflow.com/q/7320315) | Code | Helped to see how to test for a blank input in Java |
+| [Oracle Javadoc Technical Article](https://www.oracle.com/uk/technical-resources/articles/java/javadoc-tool.html) | Code | Find how to write Javadoc in code |
+| [Baeldung Javadoc](https://www.baeldung.com/javadoc) | Code | See examples of how to write Javadoc |
+| Lecture 5: Classes and Objects | Code | Where how to design and make classes and objects was shown |
+| [Github Markdown Cheatsheet](https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax) | Markdwon | Showed basic markdown syntax |
+| [Markdown Documentation](https://www.markdownguide.org/) | Markdown | Show how to write more advanced markdown |
+| [Java Documentation](https://docs.oracle.com/en/java/javase/21/docs/api/index.html) | Code | Find functions for various uses in Design.Java |
 
 <STYLE>
-* { /* Don't leave any empty lines or IntelliJ might not render correctly */
-  /* Text size */
-  font-size:   1.1rem;
-  /*font-size:   1.2rem;*/
-  /* Zenburn dark theme */
-  background-color: #2A252A;
-  color:            #D5DAD5;
+* {
+  font-size:   1.2rem;
   /* One Dark theme */
-  /*background-color: #282C34;
-  color:            #ABB2BF;*/
-  /* white-ish on dull blue-ish */
-  /*background-color: DarkSlateGray;
-    color:            AntiqueWhite;*/
-  /* white on black */
-  /*background-color: black;
-  color: white;*/
-  /* black on white */
-  /*background-color: white;
-  color: black;*/
-  /* nearly black on bright yellow */
-  /*background-color: #FFFFAA;
-  color:            #080808;*/
-  /* black on bright blue */  
-  /*background-color: #99CCFF;
-  color:            black;*/
+  background-color: #282C34;
+  color:            #ABB2BF;
 }
 body {
   /* width of the text column */
   width: 80%;
   /* line spacing */
   line-height: 180%;
-  /*line-height: 200%;*/
   /* Font styles: */
-  /* Default sans serif */
-  /*font-family: sans-serif;*/
-  /* Default serif */
-  font-family: serif;
-  /* Specific font with generic fall-back */
-  /* font-family: "Calibri Light", sans-serif; */
-  /*font-family: "OpenDyslexic", sans-serif;*/
+  font-family: "OpenDyslexic", sans-serif;
 }
 pre,
 code,
 pre code {
   /* line spacing */
   line-height: 150%;
-  /* Default monospace */
-  font-family: monospace;
-  /* Specific fixed-width font with generic fall-back */
-  /*font-family: "Consolas", monospace;*/
-  /*font-family: "OpenDyslexicMono", monospace;*/
+  font-family: "OpenDyslexicMono", monospace;
 }
 ol,
 ol ol,
@@ -66,15 +65,23 @@ ol ol ol { /* Nested lists all use decimal numbering */
 }
 em {
   /* if you want underlining instead of italics */
-  /*font-style: normal;
+  font-style: normal;
   border-bottom-style: solid;
   border-bottom-width: 1px;
-  padding-bottom:      2px;*/
-  text-decoration-skip-ink: auto;
+  padding-bottom:      2px;
 }
 h2 { /* Put a horizontal line above major headings to assist screen viewing */
   border-top:  1px solid #D5DAD5;
   margin-top:  80px;
   padding-top: 20px;
   }
+table {
+  border: 2px solid;
+}
+td,
+th {
+  border: 1px solid;
+  padding: 8px 10px;
+  width: 400px;
+}
 </STYLE>
--- Assignment2part2/src/Design.java
+++ Assignment2part2/src/Design.java
@@ -1,9 +1,222 @@
+import java.lang.Integer;
+import java.lang.Character;
+import java.lang.String;
+import java.util.Scanner;
+
 /**
- * @author TODO Your name
- * @version TODO date in YYYY-MM-DD format
+ * @author Carl Kaziboni
+ * @version 2024-03-07
  * <p>
- * One sentence summary of this class.
+ * Class to calculate income tax.
  * <p>
- * Optionally: a bit more detail about this class.
+ * Uses conditionals for each case of income tax
  */
-public class Design {}
+public class Design {
+    //tax income bands
+    private static final int PERSONAL_ALLOWANCE = 12570;
+    private static final int BAND_ONE = 37700;
+    private static final int BAND_TWO = 99730;
+    private static final int BAND_THREE = 100000;
+    private static final int BAND_FOUR = 125140;
+    //tax income rates
+    private static final double TAX_RATE_ONE = 0.2;
+    private static final double TAX_RATE_TWO = 0.4;
+    private static final double TAX_RATE_THREE = 0.45;
+    public static void main(String[] args)
+    {
+        int totalincome = 0;
+        //Get input from user
+        String input = GetNumerical.getIncome();
+        //make sure input is integer
+        try
+        {
+            totalincome = Integer.parseInt(input);
+        }
+        catch (NumberFormatException exception)
+        {
+            System.err.printf("%s not positive whole number i.e. '5000'%n", input);
+            System.exit(1);
+        }
+        finally
+        {
+            System.out.println("Income: " + totalincome);
+        }
+
+        //start calculating tax
+        int tax = 0;
+        //Non-taxable income
+        if (totalincome <= PERSONAL_ALLOWANCE)
+        {
+            printTax(tax);
+        }
+        //First 37700 after personal allowance taxed at 20%
+        else if (totalincome <= (PERSONAL_ALLOWANCE + BAND_ONE))
+        {
+            tax = calculateBandOne(totalincome);
+            printTax(tax);
+        }
+        //First 37700 after personal allowance taxed at 20% then rest of income up to 100000 taxed at 40%
+        else if (totalincome <= BAND_THREE)
+        {
+            tax = calculateBandTwo(totalincome);
+            printTax(tax);
+        }
+        //First 37700 after personal allowance taxed then rest of income taxed at 40% however after 100000 every 2 pound reduced personal allowance by 1 pound
+        else if (totalincome <= BAND_FOUR)
+        {
+            tax = calculateBandThree(totalincome);
+            printTax(tax);
+        }
+        //First 37700 taxed at 20% then next 99730 taxed at 40% then rest of income taxed 45%
+        else if (totalincome > BAND_FOUR)
+        {
+            tax = calculateBandFour(totalincome);
+            printTax(tax);
+        }
+        //If all checks were bypassed
+        else
+        {
+            System.out.println("Inappropriate format for income");
+        }
+    }
+
+
+    //Supporting methods
+    /**
+     * <p>
+     * This method prints out the tax calculated to console
+     * </p>
+     *
+     * @param tax An integer of tax calculated
+     * <p>
+     * Example of usage of printTax
+     * </p>
+     * <pre>
+     * {@code
+     * int tax = 100;
+     * printTax(tax);
+     * }
+     * </pre>
+     * <p>
+     * This prints "Tax: 100" to console
+     * </p>
+     */
+    private static void printTax(int tax)
+    {
+        System.out.println("Tax: " + tax);
+    }
+    /**
+     * <p>
+     * This method calculates the tax for salaries in band one
+     * </p>
+     * @param totalincome An integer of total income
+     * @return tax calculated
+     */
+    private static int calculateBandOne(int totalincome)
+    {
+        int tax;
+        //Calculate tax at 20%  for first 37700 after personal allowance 
+        tax = (int) ((totalincome - PERSONAL_ALLOWANCE) * TAX_RATE_ONE);
+        return tax;
+    }
+    /**
+     * <p>
+     * This method caclulates the tax for salaries in band two
+     * </p>
+     * @param totalincome An integer of total income
+     * @return tax calculated
+     */
+    private static int calculateBandTwo(int totalincome)
+    {
+        int tax;
+        //Calculate tax at 20% for first 37700 after personal allowance and remainder (as long as below or equal to 100000) at 40%.
+        tax = (int) ((BAND_ONE * TAX_RATE_ONE) + ((totalincome - (PERSONAL_ALLOWANCE + BAND_ONE)) * TAX_RATE_TWO));
+        return tax;
+    }
+    /**
+     * <p>
+     * This method calculates the tax for salaries in band three
+     * </p>
+     * @param totalincome An integer of total income
+     * @return tax calculated
+     */
+    private static int calculateBandThree(int totalincome)
+    {
+        int tax;
+        //Calculate tax at 20% for first 37700 after personal allowance and next 99730 at 40%. For every 2 pounds over 100000 1 pound is taken from personal allowance.
+        tax = (int) ((BAND_ONE * TAX_RATE_ONE) + ((totalincome - (BAND_ONE + (PERSONAL_ALLOWANCE - ((totalincome - BAND_THREE)/2)))) * TAX_RATE_TWO));
+        return tax;
+    }
+    /**
+     * <p>
+     * This method calculates the tax for salaries in band four
+     * </p>
+     * @param totalincome An integer of total income
+     * @return tax calculated
+     */
+    private static int calculateBandFour(int totalincome)
+    {
+        int tax;
+        //Calculate tax at 20% for first 37700 and next 99730 at 40% and the rest of income at 45%.
+        tax = (int) ((BAND_ONE * TAX_RATE_ONE) + (BAND_TWO * TAX_RATE_TWO) + ((totalincome - (BAND_ONE + BAND_TWO)) * TAX_RATE_THREE));
+        return tax;
+    }
+}
+
+
+/**
+ * @author Carl Kaziboni
+ * @version 2024-03-07
+ * <p>
+ * Class to get integer input.
+ * <p>
+ * Contains getIncome used to obtain integer input from user
+ */
+class GetNumerical
+{
+    /**
+     * <p>
+     * This method gets input from user
+     * </p>
+     * @return String made up of integers only
+     */
+    public static String getIncome()
+    {
+        String income;
+        boolean allNumericalInput = false;
+        boolean notEmptyInput = false;
+        System.out.print("Enter whole positive number income: ");
+        Scanner incomeInput = new Scanner(System.in);
+        //read input obtained
+        income = incomeInput.nextLine();
+        //Keep prompting for input till input is correctly given
+        while(!notEmptyInput || !allNumericalInput)
+        {
+            notEmptyInput = false;
+            allNumericalInput = false;
+            if (!income.isEmpty())
+            {
+                notEmptyInput = true;
+
+                for (int i = 0; i < income.length(); i++)
+                {
+                    if (!Character.isDigit(income.charAt(i)))
+                    {
+                        notEmptyInput = false;
+                        break;
+                    }
+                    allNumericalInput = true;
+                }
+            }
+            //all constraints satisfied
+            if (notEmptyInput && allNumericalInput)
+            {
+                break;
+            }
+            System.out.print("Please enter income in correct form (Whole positive number): ");
+            incomeInput = new Scanner(System.in);
+            income = incomeInput.nextLine();
+        }
+        return income;
+    }
+}
